234cec4f621615d1fc1b8e657ba9f6e57b3b5b42
tooru.takaya <tt.0.ayakat0@gmail.com>
2021-08-12 15:23:02 +0000

    validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @messages = Message.all
   end
@@ -28,7 +28,6 @@ class MessagesController < ApplicationController
   end
 
   def update
-    set_message
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -44,16 +43,14 @@ class MessagesController < ApplicationController
     flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
-  
+
   private
 
-　def set_message
+  def set_message
     @message = Message.find(params[:id])
   end
-  
-  # Strong Parameter
+
   def message_params
     params.require(:message).permit(:content)
   end
-
 end
