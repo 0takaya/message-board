0712895068e998591845aa57f568b9656fa400dd
tooru.takaya <tt.0.ayakat0@gmail.com>
2021-08-13 13:31:25 +0000

    add column

--- db/migrate/20210813132446_add_title_to_messages.rb
+++ db/migrate/20210813132446_add_title_to_messages.rb
@@ -1,4 +1,4 @@
-class CreateMessages < ActiveRecord::Migration[6.1]
+class AddTitleToMessages < ActiveRecord::Migration[6.1]
   def change
     add_column :messages, :title, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_133601) do
+ActiveRecord::Schema.define(version: 2021_08_13_132446) do
 
   create_table "messages", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
 end
